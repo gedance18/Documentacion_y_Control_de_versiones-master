fd5c55a7f898e46ea2a23f4036789bde356a099d
gedan <asd>
2024-04-10 10:25:21 +0200

    ADIOS

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("hola mundo");
         System.out.println("adios");
-        System.out.println("que tal??");
 
     }
 }
