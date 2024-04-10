3f818f8c11a980f3cee4693ca1732f160dc7b8e3
gedan <asd>
2024-04-10 10:59:40 +0200

    JAVADOC

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-
-
+/**
+ * Clase Main de prueba para explicar la documentacion y el control de versiones con github
+ */
 public class Main {
     public static void main(String[] args){
         System.out.println("hola mundo");
         System.out.println("adios");
         System.out.println("BUENAS");
+
     }
 }
